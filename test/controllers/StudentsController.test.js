a4a9429e9b6a947c1969582a6222fab865bfcf2e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 21:50:41 -0500

    Metodo GetEmailsStudents funcionando con su prueba

--- test/controllers/StudentsController.test.js
+++ test/controllers/StudentsController.test.js
@@ -8,4 +8,10 @@ describe("Prueba unitaria para StudentController", () =>{
         // Validamos el codigo que esperamos
         expect(students).not.toBeUndefined();
     });
+    test("Prueba 2: Recepcion estudiantes dependiendo si tienen certificacion true o false", () => {
+        const StudentsEmails = StudentController.GetEmailsStudents(true);
+        expect(StudentsEmails).toContain('Todd@visualpartnership.xyz');
+        expect(StudentsEmails).toContain('Sexton@visualpartnership.xyz');
+        expect(StudentsEmails).toContain('Sharlene@visualpartnership.xyz');
+    });
 });
