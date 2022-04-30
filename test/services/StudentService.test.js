7c78d6fe896be807ea2e8e03cea4dedf09aded5a
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 20:37:09 -0500

    Archivo StudentService con funcion obtener estudiantes con certificado con prueba

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,10 @@
+const StudentService = require("./../../lib/services/StudentService");
+const Reader = require("./../../lib/utils/Reader");
+const students = Reader.readJsonFile("visualpartners.json");
+
+describe("Prueba unitaria para StudentService", () =>{
+    test("Prueba 1: Verificar estudiantes con certificacion", () => {
+        const explorersInNode = StudentService.filterByCertification(students);
+        expect(explorersInNode).not.toBeUndefined();
+    });
+});
