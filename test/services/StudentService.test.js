94d496fe6c6dc08a661ef397863d727add19489e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 21:11:21 -0500

    Recibiendo emails dependiendo si tienen certificacion, con su prueba

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -4,7 +4,13 @@ const students = Reader.readJsonFile("visualpartners.json");
 
 describe("Prueba unitaria para StudentService", () =>{
     test("Prueba 1: Verificar estudiantes con certificacion", () => {
-        const explorersInNode = StudentService.filterByCertification(students,true);
-        expect(explorersInNode).not.toBeUndefined();
+        const StudentWCertification = StudentService.filterByCertification(students,true);
+        expect(StudentWCertification).not.toBeUndefined();
+    });
+    test("Prueba 2: Recepcion estudiantes dependiendo si tienen certificacion true o false", () => {
+        const StudentsEmails = StudentService.getEmailStudentsCertification(students,true);
+        expect(StudentsEmails).toContain('Todd@visualpartnership.xyz');
+        expect(StudentsEmails).toContain('Sexton@visualpartnership.xyz');
+        expect(StudentsEmails).toContain('Sharlene@visualpartnership.xyz');
     });
 });
