94e2008a0292626d59d338c91936b6bca0fa5902
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-29 20:06:07 -0500

    Archivo reader para obtener archivo .json con prueba de funcionamiento

---
+++ test/utils/reader.test.js
@@ -0,0 +1,11 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Prueba unitaria para Reader", () =>{
+    test("Prueba 1 Imprime lista de objetos de los estudiantes de visualpartners.json", () => {
+        // Codogigo que vamos a utlizar tal cual
+        const students = Reader.readJsonFile("visualpartners.json");
+        // Validamos el codigo que esperamos
+        expect(students).not.toBeUndefined();
+    });
+});
+module.exports = Reader;
